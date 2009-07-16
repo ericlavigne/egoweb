c256b5c1890a0aab6183033ddc8c780ab3ac47b0
Eric Lavigne <lavigne.eric@gmail.com>
2009-07-16 13:42:07 -0400

    Changes to EntityModel so that it can handle entities that have not yet been persisted.

--- src/main/net/sf/egonet/web/model/EntityModel.java
+++ src/main/net/sf/egonet/web/model/EntityModel.java
@@ -1,5 +1,7 @@
 package net.sf.egonet.web.model;
 
+import java.util.ArrayList;
+
 import net.sf.egonet.model.Entity;
 import net.sf.egonet.web.Main;
 
@@ -7,16 +9,16 @@ import org.apache.wicket.model.LoadableDetachableModel;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class EntityModel extends LoadableDetachableModel {
+public class EntityModel<E extends Entity> extends LoadableDetachableModel {
 
 	private Long id;
 	private String className;
 	
-	private transient Entity entity;
+	private transient E entity;
 	private transient Session session;
 	private transient Transaction tx;
 	
-	public EntityModel(Entity entity) {
+	public EntityModel(E entity) {
 		this.entity = entity;
 		this.id = entity.getId();
 		this.className = entity.getClass().getCanonicalName();
@@ -25,7 +27,11 @@ public class EntityModel extends LoadableDetachableModel {
 	public void save() {
 		if(entity != null) {
 			ensureSessionAvailable();
-			session.saveOrUpdate(entity);
+			if(entity.getId() == null) {
+				this.id = (Long) session.save(entity);
+			} else {
+				session.saveOrUpdate(entity);
+			}
 		}
 	}
 	
@@ -37,13 +43,18 @@ public class EntityModel extends LoadableDetachableModel {
 	}
 	
 	@Override
-	protected Entity load() {
+	protected E load() {
 		if(entity == null) {
+			if(this.id == null) {
+				return null;
+			}
 			ensureSessionAvailable();
-			entity = (Entity) 
-			session.createQuery("from "+className+" e where e.id = :id")
-			.setLong("id", id)
-			.uniqueResult();
+			ArrayList<E> entityList = 
+				new ArrayList<E>(
+					session.createQuery("from "+className+" e where e.id = :id")
+						.setLong("id", id)
+						.list());
+			entity = entityList.isEmpty() ? null : entityList.get(0);
 		}
 		
 		return entity;
