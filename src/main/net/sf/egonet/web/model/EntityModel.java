295279db572e57ea598b59875c45c80069b8c16a
Ian Taylor <ian@lorf.org>
2009-07-17 12:50:12 -0400

    merged model / breaks Main

--- src/main/net/sf/egonet/web/model/EntityModel.java
+++ src/main/net/sf/egonet/web/model/EntityModel.java
@@ -1,45 +1,49 @@
 package net.sf.egonet.web.model;
 
 import net.sf.egonet.model.Entity;
-import net.sf.egonet.web.Main;
+//import net.sf.egonet.web.Main;
 
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class EntityModel extends LoadableDetachableModel {
-
+public class EntityModel extends LoadableDetachableModel
+{
 	private Long id;
 	private String className;
-	
+
 	private transient Session session;
 	private transient Transaction tx;
-	
-	public EntityModel(Entity entity) {
+
+	public EntityModel(Entity entity)
+   	{
 		super(entity);
 		this.id = entity.getId();
 		this.className = entity.getClass().getCanonicalName();
 	}
 
-	public void save() {
+	public void save()
+   	{
 		Entity entity = (Entity) getObject();
 		if(entity != null) {
 			ensureSessionAvailable();
-			if(entity.getId() == null) {
+			if (entity.getId() == null) {
 				this.id = (Long) session.save(entity);
 			} else {
 				session.saveOrUpdate(entity);
 			}
 		}
 	}
-	
-	private void ensureSessionAvailable() {
-		if(session == null || tx == null) {
-			session = Main.getDBSessionFactory().openSession();
+
+	private void ensureSessionAvailable()
+   	{
+		if (session == null || tx == null)
+	   	{
+//			session = Main.getDBSessionFactory().openSession();
 			tx = session.beginTransaction();
 		}
 	}
-	
+
 	@Override
 	protected Object load() {
 		if(this.id == null) {
@@ -50,12 +54,13 @@ public class EntityModel extends LoadableDetachableModel {
 			.setLong("id", id)
 			.uniqueResult();
 	}
-	
-	protected void onDetach() {
-		if(tx != null) {
+
+	protected void onDetach()
+   	{
+		if (tx != null) {
 			tx.commit();
 		}
-		if(session != null) {
+		if (session != null) {
 			session.close();
 		}
 		tx = null;
