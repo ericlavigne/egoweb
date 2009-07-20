f2aa1c7f1745effa515833ddf7806881ece48a52
Ian Taylor <ian@lorf.org>
2009-07-20 19:56:10 -0400

    use generics, cleanup

--- src/main/java/net/sf/egonet/persistence/DB.java
+++ src/main/java/net/sf/egonet/persistence/DB.java
@@ -1,6 +1,7 @@
 package net.sf.egonet.persistence;
 
-import java.util.ArrayList;
+import com.google.common.collect.Lists;
+
 import java.util.List;
 
 import net.sf.egonet.model.Entity;
@@ -15,20 +16,7 @@ import org.hibernate.Transaction;
 import com.google.common.base.Function;
 
 public class DB {
-	
-	public static Object withTx(Function<Session,Object> f) {
 
-		Session session = Main.getDBSessionFactory().openSession();
-		Transaction tx = session.beginTransaction();
-		
-		Object result = f.apply(session);
-		
-		tx.commit();
-		session.close();
-		
-		return result;
-	}
-	
 	public static void save(final Entity e) {
 		withTx(new Function<Session,Object>(){
 			public Object apply(Session s) {
@@ -37,25 +25,24 @@ public class DB {
 			}
 		});
 	}
-	
+
+	//----------------------------------------
+
+	@SuppressWarnings("unchecked")
 	public static List<Study> getStudies(Session session) {
-		return new ArrayList<Study>(session.createQuery("from Study s order by s.name").list());
+		return
+			session.createQuery("from Study s order by s.name")
+				.list();
 	}
 
-	public static Entity get(Session session, String className, Long id) {
-		return (Entity) session.createQuery("from "+className+" e where e.id = :id")
-			.setLong("id", id)
-			.uniqueResult();
-	}
-	
-	public static Study getStudy(final Long studyId) {
-		return (Study) withTx(new Function<Session,Object>(){
-			public Object apply(Session session) {
-				return get(session,"Study",studyId);
+	public static Study getStudy(final Long id) {
+		return withTx(new Function<Session,Study>(){
+			public Study apply(Session session) {
+				return (Study) session.load(Study.class, id);
 			}
 		});
 	}
-	
+
 	public static List<Study> getStudies()
     {
 		Session session = Main.getDBSessionFactory().openSession();
@@ -68,16 +55,31 @@ public class DB {
 
 		return studies;
 	}
-	
+
 	public static List<Question> getQuestionsForStudy(final Long studyId, final QuestionType type) {
-		return (List<Question>) withTx(new Function<Session,Object>() {
+		return withTx(new Function<Session,List<Question>>() {
+			@SuppressWarnings("unchecked")
 			public List<Question> apply(Session session) {
-				return session.createQuery(
-						"from Question q where q.studyId = :studyId and q.typeDB = :type")
-				.setLong("studyId", studyId)
-				.setString("type", Question.typeDB(type))
-				.list();
+				return
+					session.createQuery("from Question q where q.studyId = :studyId and q.typeDB = :type")
+						.setLong("studyId", studyId)
+						.setString("type", Question.typeDB(type))
+						.list();
 			}
 		});
+	}
+
+	//----------------------------------------
+
+	private static <E> E withTx(Function<Session,E> f) {
+		Session session = Main.getDBSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+
+		E result = f.apply(session);
+
+		tx.commit();
+		session.close();
+
+		return result;
 	}
 }
