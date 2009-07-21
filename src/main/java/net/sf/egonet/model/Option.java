26e06509c41f66277f260595c5eabb288071829e
Eric Lavigne <lavigne.eric@gmail.com>
2009-07-20 20:00:50 -0400

    Hibernate mapping for the option class.

--- src/main/java/net/sf/egonet/model/Option.java
+++ src/main/java/net/sf/egonet/model/Option.java
@@ -3,22 +3,37 @@ package net.sf.egonet.model;
 /**
  * Value object representing an item of a drop-down, listbox, or similar widget
  */
-public class Option implements java.io.Serializable
+public class Option extends Entity
 {
+	private Long questionId;
 	private String name;
 
-	public Option(String name)
+	public Option(Long questionId, String name)
 	{
+		this.questionId = questionId;
 		this.name = name;
 	}
 
-//	public void setName(String name)
-//	{
-//		this.name = name;
-//	}
-
+	public Long getQuestionId() {
+		return questionId;
+	}
+	
 	public String getName()
 	{
 		return name;
+	}
+
+	// For Hibernate use only
+	protected Option() {
+		
+	}
+	// For Hibernate use only
+	protected void setQuestionId(Long questionId) {
+		this.questionId = questionId;
+	}
+	// For Hibernate use only
+	protected void setName(String name)
+	{
+		this.name = name;
 	}
 }
