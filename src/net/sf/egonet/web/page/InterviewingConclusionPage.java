4c18dc983c9d77c3101e6dfbb2a0641d62d332fc
Eric Lavigne <lavigne.eric@gmail.com>
2010-03-14 22:01:15 -0400

    This was part of Matt Futterman's earlier CASES integration changes.

--- src/net/sf/egonet/web/page/InterviewingConclusionPage.java
+++ src/net/sf/egonet/web/page/InterviewingConclusionPage.java
@@ -1,9 +1,11 @@
 package net.sf.egonet.web.page;
 
+import org.apache.wicket.Application;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
 
 import net.sf.egonet.persistence.Interviews;
 import net.sf.egonet.persistence.Studies;
+import net.sf.egonet.web.Main;
 
 public class InterviewingConclusionPage extends EgonetPage {
 	public InterviewingConclusionPage(Long interviewId) {
@@ -12,5 +14,11 @@ public class InterviewingConclusionPage extends EgonetPage {
 				+" (respondent #"+interviewId+")");
 		
         add(new MultiLineLabel("conclusion", Studies.getStudyForInterview(interviewId).getConclusion()));
+        
+        Application application = getApplication();
+        if (application instanceof Main)
+        {
+            ((Main)application).interviewHasEnded();
+        }
 	}
 }
