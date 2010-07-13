c8fc7cac6ff909be98fd11dc54656141345145c3
Kevin Neelands <Kevin Neelands>
2010-07-13 13:33:27 -0400

    Fix for crash that sometime occured when restarting an interview on an altered survey

--- src/net/sf/egonet/persistence/Interviewing.java
+++ src/net/sf/egonet/persistence/Interviewing.java
@@ -32,7 +32,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
-
+ 
 public class Interviewing {
 
 	public static Interview findOrCreateMatchingInterviewForStudy(
